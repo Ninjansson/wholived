74fe852963022ce5f0dc48564724c0c4c381b03c
Kim Jansson <kimlennart77@gmail.com>
2019-12-14 15:19:25 +0100

    Initial Commit

---
+++ people.js
@@ -0,0 +1,475 @@
+var persons = [
+{ 
+    name: "Catherine the Great",
+    title: "Empress of Russia",
+    country: ["Russia"],
+    born: 1729,
+    died: 1796,
+    wiki: "https://en.wikipedia.org/wiki/Catherine_the_Great"
+},
+{ 
+    name: "Alexander Bezborodko",
+    title: "Grand Chancellor of Russian Empire",
+    country: ["Russia"],
+    born: 1747,
+    died: 1799,
+    wiki: "https://en.wikipedia.org/wiki/Alexander_Bezborodko"
+},
+{ 
+    name: "Nikita Ivanovich Panin",
+    title: "Russian statesman and political mentor to Catherine the Great",
+    country: ["Russia"],
+    born: 1718,
+    died: 1783,
+    wiki: "https://en.wikipedia.org/wiki/Nikita_Ivanovich_Panin"
+},
+{ 
+    name: "Daikokuya Kōdayū",
+    title: "Japanese castaway",
+    country: ["Russia"],
+    born: 1751,
+    died: 1828,
+    wiki: "https://en.wikipedia.org/wiki/Daikokuya_K%C5%8Dday%C5%AB"
+},
+{ 
+    name: "Gabriela Silang",
+    title: "Rebel leader and heroine",
+    country: ["Philippines"],
+    born: 1731,
+    died: 1763,
+    wiki: "https://en.wikipedia.org/wiki/Gabriela_Silang"
+},
+{
+    name: "Erik Laxmann",
+    title: "Clergyman, explorer and natural scientist",
+    country: ["Finland", "Sweden"],
+    born: 1737,
+    died: 1796,
+    wiki: "https://en.wikipedia.org/wiki/Erik_Laxmann"
+},
+{
+    name: "Grigory Potemkin",
+    title: "Military leader, statesman and nobleman",
+    country: ["Russia"],
+    born: 1739,
+    died: 1791,
+    wiki: "https://en.wikipedia.org/wiki/Grigory_Potemkin"
+},
+{
+    name: "Henry Cavendish",
+    title: "Scientist",
+    country: ["England"],
+    born: 1731,
+    died: 1810,
+    wiki: "https://en.wikipedia.org/wiki/Henry_Cavendish"
+},
+{
+    name: "Abd al-Ghani al-Nabulsi",
+    title: "Academic",
+    country: ["Ottoman Empire"],
+    born: 1641,
+    died: 1731,
+    wiki: "https://en.wikipedia.org/wiki/Abd_al-Ghani_al-Nabulsi"
+},
+{
+    name: "Jonas Alströmer",
+    title: "Pioneer of agriculture and industry",
+    country: ["Sweden"],
+    born: 1685,
+    died: 1761,
+    wiki: "https://en.wikipedia.org/wiki/Jonas_Alstr%C3%B6mer"
+},
+{
+    name: "Anthony van Dyck",
+    title: "Painter",
+    country: ["Flemish"],
+    born: 1599,
+    died: 1641,
+    wiki: "https://en.wikipedia.org/wiki/Anthony_van_Dyck"
+},
+{
+    name: "Jesus of Nazareth",
+    title: "Jewish preacher and religious leader.",
+    country: ["Judean"],
+    born: -4,
+    died: 30,
+    wiki: "https://en.wikipedia.org/wiki/Jesus"
+},
+{
+    name: "Ming of Han",
+    title: "Chinese emperor",
+    country: ["China"],
+    born: 28,
+    died: 75,
+    wiki: "https://en.wikipedia.org/wiki/Emperor_Ming_of_Han"
+},
+{
+    name: "Augustus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: -63,
+    died: 14,
+    wiki: "https://en.wikipedia.org/wiki/Augustus"
+},
+{
+    name: "Tiberius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: -42,
+    died: 37,
+    wiki: "https://en.wikipedia.org/wiki/Tiberius"
+},
+{
+    name: "Caligula",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 12,
+    died: 41,
+    wiki: "https://en.wikipedia.org/wiki/Caligula"
+},
+{
+    name: "Claudius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: -10,
+    died: 54,
+    wiki: "https://en.wikipedia.org/wiki/Claudius"
+},
+{
+    name: "Nero",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 37,
+    died: 68,
+    wiki: "https://en.wikipedia.org/wiki/Nero"
+},
+{
+    name: "Galba",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: -3,
+    died: 69,
+    wiki: "https://en.wikipedia.org/wiki/Galba"
+},
+{
+    name: "Otho",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 32,
+    died: 69,
+    wiki: "https://en.wikipedia.org/wiki/Otho"
+},
+{
+    name: "Vitellius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 15,
+    died: 69,
+    wiki: "https://en.wikipedia.org/wiki/Vitellius"
+},
+{
+    name: "Vespasian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 9,
+    died: 79,
+    wiki: "https://en.wikipedia.org/wiki/Vespasian"
+},
+{
+    name: "Titus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 39,
+    died: 81,
+    wiki: "https://en.wikipedia.org/wiki/Titus"
+},
+{
+    name: "Domitian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 51,
+    died: 96,
+    wiki: "https://en.wikipedia.org/wiki/Domitian"
+},
+{
+    name: "Nerva",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 30,
+    died: 98,
+    wiki: "https://en.wikipedia.org/wiki/Nerva"
+},
+{
+    name: "Trajan",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 53,
+    died: 117,
+    wiki: "https://en.wikipedia.org/wiki/Trajan"
+},
+{
+    name: "Hadrian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 76,
+    died: 138,
+    wiki: "https://en.wikipedia.org/wiki/Hadrian"
+},
+{
+    name: "Antoninus Pius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 86,
+    died: 161,
+    wiki: "https://en.wikipedia.org/wiki/Antoninus_Pius"
+},
+{
+    name: "Marcus Aurelius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 121,
+    died: 180,
+    wiki: "https://en.wikipedia.org/wiki/Marcus_Aurelius"
+},
+{
+    name: "Lucius Verus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 130,
+    died: 169,
+    wiki: "https://en.wikipedia.org/wiki/Lucius_Verus"
+},
+{
+    name: "Commodus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 161,
+    died: 192,
+    wiki: "https://en.wikipedia.org/wiki/Commodus"
+},
+{
+    name: "Pertinax",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 126,
+    died: 193,
+    wiki: "https://en.wikipedia.org/wiki/Pertinax"
+},
+{
+    name: "Didius Julianus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 133,
+    died: 193,
+    wiki: "https://en.wikipedia.org/wiki/Didius_Julianus"
+},
+{
+    name: "Septimius Severus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 145,
+    died: 211,
+    wiki: "https://en.wikipedia.org/wiki/Septimius_Severus"
+},
+{
+    name: "Caracalla",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 188,
+    died: 217,
+    wiki: "https://en.wikipedia.org/wiki/Caracalla"
+},
+{
+    name: "Geta",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 189,
+    died: 211,
+    wiki: "https://en.wikipedia.org/wiki/Geta_(emperor)"
+},
+{
+    name: "Macrinus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 165,
+    died: 218,
+    wiki: "https://en.wikipedia.org/wiki/Macrinus"
+},
+{
+    name: "Diadumenian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 208,
+    died: 218,
+    wiki: "https://en.wikipedia.org/wiki/Diadumenian"
+},
+{
+    name: "Elagabalus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 204,
+    died: 222,
+    wiki: "https://en.wikipedia.org/wiki/Elagabalus"
+},
+{
+    name: "Severus Alexander",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 208,
+    died: 235,
+    wiki: "https://en.wikipedia.org/wiki/Severus_Alexander"
+},
+{
+    name: "Maximinus Thrax",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 173,
+    died: 238,
+    wiki: "https://en.wikipedia.org/wiki/Maximinus_Thrax"
+},
+{
+    name: "Pupienus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 165,
+    died: 238,
+    wiki: "https://en.wikipedia.org/wiki/Pupienus"
+},
+{
+    name: "Balbinus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 178,
+    died: 238,
+    wiki: "https://en.wikipedia.org/wiki/Balbinus"
+},
+{
+    name: "Gordian I",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 159,
+    died: 238,
+    wiki: "https://en.wikipedia.org/wiki/Gordian_I"
+},
+{
+    name: "Gordian II",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 192,
+    died: 238,
+    wiki: "https://en.wikipedia.org/wiki/Gordian_II"
+},
+{
+    name: "Gordian III",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 225,
+    died: 244,
+    wiki: "https://en.wikipedia.org/wiki/Gordian_III"
+},
+{
+    name: "Philip the Arab",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 204,
+    died: 249,
+    wiki: "https://en.wikipedia.org/wiki/Philip_the_Arab"
+},
+{
+    name: "Philip II",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 237,
+    died: 249,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Philip_II_(Roman_Emperor)"
+},
+{
+    name: "Decius",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 201,
+    died: 251,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Decius"
+},
+{
+    name: "Herennius Etruscus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 227,
+    died: 251,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Herennius_Etruscus"
+},
+{
+    name: "Hostilian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 230,
+    died: 251,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Hostilian"
+},
+{
+    name: "Trebonianus Gallus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 206,
+    died: 253,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Trebonianus_Gallus"
+},
+{
+    name: "Volusianus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 230,
+    died: 253,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Volusianus"
+},
+{
+    name: "Aemilianus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 207,
+    died: 253,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Aemilianus"
+},
+{
+    name: "Valerian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 193,
+    died: 264,
+    wiki: "https://en.wikipedia.org/wiki/Valerian_(emperor)"
+},
+{
+    name: "Gallienus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 218,
+    died: 268,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Gallienus"
+},
+{
+    name: "Claudius Gothicus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 214,
+    died: 270,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Claudius_Gothicus"
+},
+/*{
+    name: "Quintillus",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 214,
+    died: 270,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Quintillus"
+}*/
+
+{
+    name: "Aurelian",
+    title: "Emperor of the Roman Empire",
+    country: ["Roman Empire"],
+    born: 214,
+    died: 275,
+    wiki: "https://en.wikipedia.org/wiki/https://en.wikipedia.org/wiki/Aurelian"
+}
+];
